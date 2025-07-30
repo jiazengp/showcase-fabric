3909f59de0f799542e659d4c371c7cd10a079d50
Hyouka <jiazengp@gmail.com>
2025-07-30 16:01:59 +0800

    style: format code

--- src/main/java/com/showcase/utils/TextUtils.java
+++ src/main/java/com/showcase/utils/TextUtils.java
@@ -12,11 +12,11 @@ public class TextUtils {
     public static final Text BADGE = Text.literal(ShowcaseMod.MOD_ID).formatted(Formatting.BOLD, Formatting.ITALIC);
     public static final Text UNKNOWN_ENTRY =  Text.translatable("commands.banlist.entry.unknown");
     public static final Text UNKNOW_PLAYER = Text.translatable("argument.player.unknown");
-   public static final Text INVENTORY  = Text.translatable("key.categories.inventory");
-   public static final Text HOTBAR  = Text.translatable("options.attack.hotbar");
-   public static final Text ENDER_CHEST = Text.translatable("container.enderchest");
-   public static final Text EMPTY = Text.translatable("item.minecraft.bundle.empty");
-   public static final Text CONTAINER = Text.translatable("showcase.screen.container_title");
+    public static final Text INVENTORY  = Text.translatable("key.categories.inventory");
+    public static final Text HOTBAR  = Text.translatable("options.attack.hotbar");
+    public static final Text ENDER_CHEST = Text.translatable("container.enderchest");
+    public static final Text EMPTY = Text.translatable("item.minecraft.bundle.empty");
+    public static final Text CONTAINER = Text.translatable("showcase.screen.container_title");
 
     public static Text error(Text msg) {
         return Text.literal("").append(msg).formatted(Formatting.RED);
